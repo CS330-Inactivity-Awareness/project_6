2b3b6e40171e254f0556eb60bdec3314e2e82b6a
John Laboe <johncc97@gmail.com>
2019-11-14 13:27:05 -0600

    style changes

--- screens/ReminderSetup.js
+++ screens/ReminderSetup.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View, Button, Picker, TextInput } from 'react-native';
+import { Platform, StyleSheet, Text, View, Button, Picker, TextInput, Keyboard, TouchableWithoutFeedback} from 'react-native';
 export default class ProfileScreen extends React.Component {
   constructor(props){
     super(props);
-    this.state = {btype: "exercise", work_period: (1).toString(), break_period: (1).toString()};
+    this.state = {btype: "exercise", work_period: '', break_period: ''};
     this.onChangeWorkingPeriod = this.onChangeWorkingPeriod.bind(this);
     this.onChangeBreakPeriod = this.onChangeBreakPeriod.bind(this);
   }
@@ -17,74 +17,116 @@ export default class ProfileScreen extends React.Component {
   }
 
   static navigationOptions = {
-    title: 'Welcome',
+    title: 'Reminder Creation',
+    headerStyle: {
+      backgroundColor: '#4444f0',
+      
+    },
+    headerTintColor: '#fff'
   };
+
   render() {
     const {navigate} = this.props.navigation;
     return (
-      <View>
-        <Text style = {styles.work_period}>
-          Work Period Length (minutes):
-        </Text>
-        <TextInput
-          style={styles.type_box}
-          onChangeText={text => this.onChangeWorkingPeriod(text)}
-          value={this.state.work_period}>
-        </TextInput>
-        <Text style = {styles.work_period}>
-          Break Length (minutes):
-        </Text>
-        <TextInput
-          style={ styles.type_box  }
-          onChangeText={text => this.onChangeBreakPeriod(text)}
-          value={this.state.break_period}>
-        </TextInput>
-        <Text style = {styles.work_period}>
-          Break Type:
-        </Text>
-        <Picker
-          selectedValue={this.state.btype}
-          style={{height: 50, width: 100, left: 30, }}
-          onValueChange={(itemValue, itemIndex) =>
-            this.setState({btype: itemValue})
-          }>
-          <Picker.Item label="Exercise" value="exercise" />
-          <Picker.Item label="Stretch" value="strech" />
-          <Picker.Item label="Free Time" value="free_time" />
-        </Picker>
-        <Button
-        title="Set Reminder"
-        onPress={() => navigate('Reminder', {name: 'Jane', work_period: this.state.work_period, break_period: this.state.break_period})}
-        style = {styles.button}
-      />
+      <TouchableWithoutFeedback onPress={()=> Keyboard.dismiss()}> 
+      <View style = {styles.container}>
+
+        <View style={styles.input_row}>
+          <Text style = {styles.input_text}>
+            Work Period Length (minutes)
+          </Text>
+
+          <TextInput
+            style={styles.type_box}
+            onChangeText={text => this.onChangeWorkingPeriod(text)}
+            value={this.state.work_period}
+            keyboardType='numeric'>
+          </TextInput>
+        </View>
+        
+        <View style={styles.input_row}>
+          <Text style = {styles.input_text}>
+            Break Length (minutes)
+          </Text>
+
+          <TextInput
+            style={ styles.type_box  }
+            onChangeText={text => this.onChangeBreakPeriod(text)}
+            value={this.state.break_period}
+            keyboardType='numeric'>
+          </TextInput>
+        </View>
+        <View style={styles.input_row}>
+          <Text style = {styles.input_text}>
+            Break Type
+          </Text>
+
+          <Picker
+            selectedValue={this.state.btype}
+            style={{height: 50, width: '80%', flex: 1, borderColor: 'gray', borderWidth: 1}}
+            onValueChange={(itemValue, itemIndex) =>
+              this.setState({btype: itemValue})
+            }>
+            <Picker.Item label="Exercise" value="exercise" />
+            <Picker.Item label="Stretch" value="strech" />
+            <Picker.Item label="Free Time" value="free_time" />
+          </Picker>
+        </View>
+        <View style={styles.button_view}>
+          <Button
+            title="Set Reminder"
+            onPress={() => navigate('Reminder', {name: 'Jane', work_period: this.state.work_period, break_period: this.state.break_period})}
+            style = {styles.button}
+            color = "#4444f0"
+          />
+        </View>
       </View>
+      </TouchableWithoutFeedback>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  work_period: {
-    margin: 5,
-    fontSize: 16,
-    left: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+  container: {
+    flexDirection: 'column',
+    flex: 1,
+    justifyContent: 'space-evenly'
   },
+  
+  input_text: {
+    flex: 1,
+    textAlign: 'left',
+    fontSize: 20,
+  },
+
+
   type_box: {
-     height: 40,
      borderColor: 'gray',
      borderWidth: 1,
-     width:150,
-     left: 30,
-     marginBottom: 20
+     height: '40%',
+     width: '20%',
+     textAlign: 'center',
+     fontSize: 20,
+     flex: 1,
   },
+
   button: {
     textAlign: 'center',
-    color: '#333333',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: 150,
-    height: 50,
-    top: 50,
+    color: '#4444f0',
+    height: '100%'
   },
+
+  input_row: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingLeft: '7%',
+    paddingRight: '7%',
+  },
+
+  button_view: {
+    flex: 1
+  }
+
+
 });
